41b90a6e8f0fde4c610f370748901cc3dd355587
Larissa Marques <larissamarques@live.com.pt>
2022-10-03 16:03:49 +0100

    Add contact input

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import Done from "@mui/icons-material/Done";
+import Typography from "@mui/material/Typography";
 
-export default () => {
+
+function Contact() {
     return (
-        <div>
-            <h1>contact</h1>
-        </div>
+        <Box sx={{ flexGrow: 1, padding: 5 }}>
+            <Typography variant="h6" gutterBottom>
+                Quer saber mais?
+            </Typography>
+            <Typography variant="overline" gutterBottom>
+                Nos deixe o seu contacto!
+            </Typography>
+            <Grid container spacing={2} minHeight={80}>
+                <Grid display="flex" justifyContent="center" alignItems="center">
+                    <TextField
+                        id="standard-basic"
+                        label="Deixe o seu contacto"
+                        variant="standard"
+                        placeholder="(351) 999999999"
+                    />
+                    <IconButton color="primary" aria-label="Deixe o seu contacto">
+                        <Done />
+                    </IconButton>
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
+export default Contact;
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -4,13 +4,18 @@ import './styles.css';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import CompareSpeed from '../../components/CompareSpeed';
+import Contact from '../../components/Contact';
+import Container from '@mui/material/Container';
 
 function Home() {
   return (
     <div>
-       <Header />
-       <CompareSpeed />
-       <Footer />
+      <Header />
+      <Container maxWidth="sm">
+        <CompareSpeed />
+        <Contact />
+      </Container>
+      <Footer />
     </div>
   );
 }
